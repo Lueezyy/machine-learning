3fa0e94023a162f41339caaa5e9fcd3c4d4f8293
Lueezyy <luis.balb@icloud.com>
2026-05-13 00:38:10 -0500

    Add description to adaline

--- adaline.py
+++ adaline.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 class AdalineGD:
+    '''ADAptive LInear NEuron classifier.'''
     def __init__(self, learning_rate=0.01, n_iterations=50, random_state=1):
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
