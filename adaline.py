80d881f461665ffe2a391a6a3d10b6cee27beca7
Lueezyy <luis.balb@icloud.com>
2026-05-13 00:43:44 -0500

    Add fit method

--- adaline.py
+++ adaline.py
@@ -6,3 +6,10 @@ class AdalineGD:
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float64(0.0)
+        self.losses_ = []
+        return self
