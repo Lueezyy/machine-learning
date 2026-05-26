88ec5f833bf0e719edc3201c440df927da356262
Lueezyy <luis.balb@icloud.com>
2026-05-25 21:31:57 -0500

    fit function

--- logregression.py
+++ logregression.py
@@ -12,11 +12,16 @@ class LogisticRegressionGD:
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
         self.b_ = np.float64(0.0)
-        self.losses = []
+        self.losses_ = []
 
         for i in range(self.n_iter):
             net_input = self.net_input(X)
             output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (-y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output))) / X.shape[0])
+            self.losses_.append(loss)
         return self
 
     def net_input(self, X):
